c9455665cdd3bc99131386275f300692acec949f
amaiahe <amaia.hiribarne@timeonegroup.com>
2017-09-28 18:23:18 +0200

    update header

--- view/templates/header.php
+++ view/templates/header.php
@@ -13,13 +13,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Hello World ! - Game</title>
+    <title>Blog MVC</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 
     <!--  Style.css  -->
-    <link href="<?php echo URL_CSS ?>/style.css" rel="stylesheet">
+    <!--<link href="/style.css" rel="stylesheet">-->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
