a5040bc7ac3c3852efcbc5e1c78df74cc2d47394
amaiahe <amaia.hiribarne@timeonegroup.com>
2017-09-28 18:22:03 +0200

    ajout template html de la page account

---
+++ view/account.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <div class="panel-body">
+        <h1>Mon compte</h1>
+
+        <table>
+            <tr>
+                <th>Article</th>
+                <th>Date parution</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</div>
