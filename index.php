850e213a962257b79eb36dc1003fa9408a4717d4
amaiahe <amaia.hiribarne@timeonegroup.com>
2017-09-28 18:28:17 +0200

    add memoire tampon

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
+session_start();
+ob_start();
 require_once('view/templates/header.php');
 require_once('view/templates/menu.php');
 
+
+$contents = ob_get_contents();
+ob_end_clean();
 
 if(!isset($_REQUEST['module']) ){
      $_REQUEST['module'] = 'accueil';
@@ -10,19 +15,18 @@ $module = $_REQUEST['module'];
 
 switch($module){
     case 'accueil':
-                 //ob_end_flush();
-                include("view/index.php") ;
-                break;
+        echo $contents;
+        include("view/index.html");
+        break;
 
-     case 'user':
-                //ob_end_flush();
-                include("Controllers/UsersController.php") ;
-                break;
+    case 'user':
+        include("Controllers/UsersController.php");
+        break;
 
-     case 'account':
-                 //ob_end_flush();
-                include("view/account.php") ;
-                break;
+    case 'account':
+        echo $contents;
+        include("view/account.php") ;
+        break;
 
 
 
