429dd986ca27f971ae9063b700912d451032a28b
Ythiss <amaia.hiribarne@gmail.com>
2017-10-02 01:00:42 +0200

    test for user infos

--- view/account.php
+++ view/account.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="panel-body">
         <h1>Mon compte</h1>
-
+        <p>Username : <?php var_dump($_SESSION['username']);?></p>
         <table>
             <tr>
                 <th>Article</th>
